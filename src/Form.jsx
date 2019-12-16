95e0e6e06e0153e70fab01fc4b4c8dff872ecd44
mustafa-al-shaheri <mustafa.alshaheri@gmail.com>
2019-12-16 05:04:21 -0800

    Create Form.jsx
    add

---
+++ src/Form.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+export default class Form extends Component {
+  render() {
+    return (
+      <fieldset>
+        <legend>ADD TODO</legend>
+        <form onSubmit={this.props.handleSubmit}>
+          <input
+            type="text"
+            value={this.props.value}
+            onChange={this.props.handleChange2}
+          />
+          <button>ADD</button>
+        </form>
+      </fieldset>
+    );
+  }
+}
